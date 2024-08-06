afdcb8ea55481b73620afe9480c5379671237bbf
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-08-05 21:19:52 -0600

    Fixed admin player

--- ttranking/players/admin.py
+++ ttranking/players/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Player
 
 
+@admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'alias', 'gender', 'date_of_birth', 'nationality', 'ranking', 'created_at', 'updated_at')
     search_fields = ('first_name', 'last_name', 'alias', 'nationality')
@@ -15,5 +16,3 @@ class PlayerAdmin(admin.ModelAdmin):
             obj.updated_by = request.user
         super().save_model(request, obj, form, change)
 
-
-admin.site.register(Player, PlayerAdmin)
