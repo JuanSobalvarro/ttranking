35ffe0fdefc0f24c395b8e26aa3a39dba8031e9c
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-08-05 21:17:57 -0600

    Added Player Model to Admin

--- ttranking/players/admin.py
+++ ttranking/players/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# ttranking/players/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Player
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'alias', 'gender', 'date_of_birth', 'nationality', 'ranking', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'alias', 'nationality')
+    list_filter = ('gender', 'nationality')
+    ordering = ('-ranking', 'last_name')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Player, PlayerAdmin)
