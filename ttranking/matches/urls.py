0554acce99acb84a4ec8ec81f52172188528eb6d
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-08-05 21:28:08 -0600

    Fixed urls app names

--- ttranking/matches/urls.py
+++ ttranking/matches/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'matches'
+
 urlpatterns = [
     path('', views.match_list, name='match_list'),
     path('<int:match_id>/', views.match_detail, name='match_detail'),
