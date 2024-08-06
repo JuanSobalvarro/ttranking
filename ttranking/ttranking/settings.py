27fd26a06e3d4dfd1ca79d370cbb1b4547e54dd4
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-08-06 09:55:54 -0600

    Fixing views

--- ttranking/ttranking/settings.py
+++ ttranking/ttranking/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xfbsaylhh*wxxlzs8x!kclhcdvp+jqo!fudbs(ni266o%iq*_k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '0.0.0.0', '[::1]', 'ttranking.juansobalvarro.site']
 
