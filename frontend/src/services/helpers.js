ac3a267dac240a5b6e14fd4fd731a4d7e44665e5
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-02-06 22:36:11 -0600

    Get player image update

--- frontend/src/services/helpers.js
+++ frontend/src/services/helpers.js
@@ -1,4 +1,4 @@
-const url = import.meta.env.VITE_API_URL || 'http://localhost:8000/';;
+const url = import.meta.env.VITE_MEDIA_URL || 'http://localhost:8000/';
 
 export const getPlayerFullname = (player) => {
     if (!player) {
